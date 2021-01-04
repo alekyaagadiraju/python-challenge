3b5f9e1f17b0aa2ee21a417163b43cb308919413
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-01-04 15:29:42 -0500

    modified file

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,8 +9,8 @@ csvpath = os.path.join("Resources", "budget_data.csv" )
 month_count = 0
 Total_Profit_Loss = 0
 Difference = 0
-DiffMax = 0
-DiffMin = 0
+Diff_Max = 0
+Diff_Min = 0
 initial_profit = 0
 
 # Create lists for Profit total and Average Profit change
@@ -33,20 +33,25 @@ with open(csvpath) as csvfile:
         Total = i[1]
         New_profit = int(Total)
         Diff = New_profit - initial_profit
-        Monthly_Change = New_profit - initial_profit
+        Difference_Total.append((Diff))
 
     # Total Profit List Change
         Total_Profit.append(int(Total))
+
         
     # Greatest increase in profits over period
-        if DiffMax < Diff:
-            DiffMax = Diff
+        if Diff_Max < Diff:
+            Diff_Max = Diff
             DiffMaxDate = month
 
+            
+
     # Greatest decrease in losses over period
-        if DiffMin > Diff:
-            DiffMin = Diff
+        if Diff_Min > Diff:
+            Diff_Min = Diff
             DiffMinDate = month
+
+    
 
     # Set New Profit to get changes
         initial_profit = New_profit
@@ -54,13 +59,26 @@ with open(csvpath) as csvfile:
 # Sum of Total Profit/Loss
 Sum_Profit = sum(Total_Profit)
 
+#Average of Monthly Changes
+Avg_Profit_Change = sum(Difference_Total)/ len(Difference_Total)
 
 # Print Financial Analysis
 print("Financial Analysis")
 print("------------------------------------")
 print(f'Total Months : {month_count}')
 print(f'Total: $ {Sum_Profit}')
-print(f'Greatest Increase in Profits: {DiffMaxDate} : ($ {DiffMax})')
-print(f'Greatest Decrease in Profits: {DiffMinDate} : ($ {DiffMin})')
+print(f'Average Change: {Avg_Profit_Change}')
+print(f'Greatest Increase in Profits: {DiffMaxDate} : ($ {Diff_Max})')
+print(f'Greatest Decrease in Profits: {DiffMinDate} : ($ {Diff_Min})')
+
+with open('financial_analysis.txt', 'w') as text:
+    text.write("  Financial Analysis"+ "\n")
+    text.write("----------------------------------------------------------\n\n")
+    text.write("    Total Months: " + str(month_count) + "\n")
+    text.write("    Total Profits: " + "$" + str(Sum_Profit) +"\n")
+    text.write("    Average Change: " + '$' + str(int(Avg_Profit_Change)) + "\n")
+    text.write("    Greatest Increase in Profits: " + str(DiffMaxDate) + " ($" + str(Diff_Max) + ")\n")
+    text.write("    Greatest Decrease in Profits: " + str(DiffMinDate) + " ($" + str(Diff_Min) + ")\n")
+    
 
 
