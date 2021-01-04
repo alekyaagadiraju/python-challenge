be3afa6b2c4fd99c96d874d7a74874323a478719
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-01-04 14:07:56 -0500

    correct analysis with average error

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,16 +3,19 @@ import os
 import csv
 
 # Set file path
-csvpath = os.path.join("..", "Resources","budget_data.csv" )
+csvpath = os.path.join("Resources", "budget_data.csv" )
 
 # Set variables
 month_count = 0
-Total_P/L = 0
+Total_Profit_Loss = 0
 Difference = 0
 Max_Difference = 0
 Min_Difference = 0
 initial_profit = 0
-Difference_Total = 0
+
+# Create list for Total Profit
+Total_Profit = []
+Difference_Total = []
 
 # Open and read csv
 with open(csvpath) as csvfile:
@@ -20,41 +23,48 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
 # Calculate analysis
-for i in csvreader:
+    for i in csvreader:
     # Total months
-    month_count = month_count + 1
+        month_count = month_count + 1
 
     # Total Profits/Losses
-    Total = i[1]
-    Total_P/L += int(Total)
+        Total = i[1]
+        Total_Profit.append(int(Total))
 
-    # Set Profit/Loss Change Variables
-    New_profit = int(Total)
-    Difference = New_profit - initial_profit
-    Monthly_Change = New_profit - initial_profit
-    Difference_Total = Difference_Total + Monthly_Change
-
-    # Average Profit/Loss Change
-    Avg_Profit_Change = (Difference_Total/month_count)
+    #Set monthly change variables
+        month = i[0]
+        New_profit = int(i[1])
+        Difference = New_profit - initial_profit
+        Monthly_Change = New_profit - initial_profit
+        
+    # Monthly Profit/Loss Change
+        Difference_Total.append(int(Monthly_Change)
 
     # Greatest increase in profits over period
-    if Max_Difference < Difference:
-            Max_Difference = Difference
-            DiffMaxDate = month
+        if Max_Difference < Difference:
+             Max_Difference = Difference
+             DiffMaxDate = month
 
     # Greatest decrease in losses over period
-    if Min_Difference > Difference:
-            Min_Difference = Difference
-            DiffMinDate = month
+        if Min_Difference > Difference:
+             Min_Difference = Difference
+             DiffMinDate = month
 
     # Set New Profit to get changes
-    initial_profit = New_profit
+        initial_profit = New_profit
+
+# Sum of Total Profit/Loss
+Sum_Profit = sum(Total_Profit)
+
+#Average of Monthly Changes
+Avg_Profit_Change = ((sum(Difference_Total))/month_count)
 
 # Print Financial Analysis
 print("Financial Analysis")
 print("------------------------------------")
 print(f'Total Months : {month_count}')
-print(f'Total: $ {Total}')x
+print(f'Total: $ {Sum_Profit}')
 print(f'Average Change: {Avg_Profit_Change}')
 print(f'Greatest Increase in Profits: {DiffMaxDate} : ($ {Max_Difference})')
 print(f'Greatest Decrease in Profits: {DiffMinDate} : ($ {Min_Difference})')
+
