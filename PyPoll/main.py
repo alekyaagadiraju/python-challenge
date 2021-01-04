4c8f8a57be9dd6be57f3df709e835934bca4a74e
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-01-04 17:16:54 -0500

    Calculated percentages

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,6 +11,7 @@ voter_count = 0
 # List of voters
 candidate_list = []
 voter_countlist = []
+candidate_percent = []
 
 # Open and read csv
 with open(csvpath) as csvfile:
@@ -31,19 +32,26 @@ with open(csvpath) as csvfile:
             #else add another vote for this candidate
         else:
             indexofCandidate =  candidate_list.index(candidate)
-            curVoteTally = voter_countlist[indexofCandidate]
+            VoteTally = voter_countlist[indexofCandidate]
             #increase vote tally for this candidate by 1
-            voter_countlist[indexofCandidate] = curVoteTally+1
+            voter_countlist[indexofCandidate] = VoteTally+1
+            
+    #compute percentage
+            
+    for candidate in candidate_list:
+            votes = voter_countlist[candidate_list.index(candidate)]
+            percent_of_votes = (votes/voter_count)*100
+            candidate_percent.append(percent_of_votes)
 
 
-
+print(f'{candidate_percent}')
 # Print Election Results
 print("Election Results")
 print("------------------------------------")
 print(f'Total Votes : {voter_count}')
 print("------------------------------------")
-print(f'{candidate_list[0]} : ({voter_countlist[0]})') 
-print(f'{candidate_list[1]} : ({voter_countlist[1]})') 
-print(f'{candidate_list[2]} : ({voter_countlist[2]})') 
-print(f'{candidate_list[3]} : ({voter_countlist[3]})') 
+print(f'{candidate_list[0]} : {candidate_percent[0]}% ({voter_countlist[0]})') 
+print(f'{candidate_list[1]} : {candidate_percent[1]}% ({voter_countlist[1]})') 
+print(f'{candidate_list[2]} : {candidate_percent[2]}% ({voter_countlist[2]})') 
+print(f'{candidate_list[3]} : {candidate_percent[3]}% ({voter_countlist[3]})') 
 print("------------------------------------")
